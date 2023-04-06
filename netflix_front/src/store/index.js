68cfe24917dd30dfd65176c4bd971280a44f484e
gowtham-source <75899822+gowtham-source@users.noreply.github.com>
2023-04-07 01:18:10 +0530

    updated backend link

--- netflix_front/src/store/index.js
+++ netflix_front/src/store/index.js
@@ -81,7 +81,9 @@ export const getUsersLikedMovies = createAsyncThunk(
   async (email) => {
     const {
       data: { movies },
-    } = await axios.get(`http://localhost:5000/api/user/liked/${email}`);
+    } = await axios.get(
+      `https://netflix-clone-zdbs.onrender.com/api/user/liked/${email}`
+    );
     return movies;
   }
 );
@@ -91,10 +93,13 @@ export const removeMovieFromLiked = createAsyncThunk(
   async ({ movieId, email }) => {
     const {
       data: { movies },
-    } = await axios.put("http://localhost:5000/api/user/remove", {
-      email,
-      movieId,
-    });
+    } = await axios.put(
+      "https://netflix-clone-zdbs.onrender.com/api/user/remove",
+      {
+        email,
+        movieId,
+      }
+    );
     return movies;
   }
 );
